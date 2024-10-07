a066196f9c540d4b09fabec1c2754ffe4ebd1017
Diego63212 <doom63212@gmail.com>
2024-10-07 16:59:48 -0300

    Implement game end and reset

--- js/script.js
+++ js/script.js
@@ -1,11 +1,15 @@
 function Gameboard () {
     const board = [];
     const size = 3;
+    let filledCells = 0;
 
-    for (let i = 0; i < size; i++) {
-        board[i] = [];
-        for (let j = 0; j < size; j++) {
-            board[i].push(Cell());
+    const createBoard = () => {
+        filledCells = 0;
+        for (let i = 0; i < size; i++) {
+            board[i] = [];
+            for (let j = 0; j < size; j++) {
+                board[i].push(Cell());
+            }
         }
     }
 
@@ -21,9 +25,14 @@ function Gameboard () {
 
     const placeToken = function (row, column, player) {
         board[row][column].placeToken(player);
+        filledCells++;
     }
 
-    return { printBoard, getCell, placeToken, getSize: board.length };
+    const getSize = () => size;
+
+    const getFilledCells = () => filledCells;
+    createBoard();
+    return { printBoard, getCell, placeToken, createBoard, getSize, getFilledCells };
 }
 
 function Player (name, token, id) {
@@ -47,6 +56,7 @@ function Cell () {
 function GameController () {
     const board = Gameboard();
     const players = [Player('One', 'X', 1), Player('Two', 'O', 20)];
+    let isGameFinished = false;
 
     let currentPlayer = players[0];
     
@@ -59,17 +69,25 @@ function GameController () {
     }
     
     const playRound = (row, column) => {
+        if (isGameFinished) {
+            return resetGame()
+        }
         if (board.getCell(row, column)) return console.log('Cell already occupied');
 
         board.placeToken(row, column, currentPlayer);
         console.log(board.printBoard());
         checker();
+        if (isGameFinished) {
+            return true;
+        }
         changePlayer();
         console.log(`Player ${currentPlayer.name} turn`);
     }
 
     const resetGame = () => {
-
+        isGameFinished = false;
+        currentPlayer = players[0];
+        board.createBoard();
     }
     
     const getPlayer = () => currentPlayer;
@@ -77,8 +95,14 @@ function GameController () {
     console.log(`Player ${currentPlayer.name} turn`);
 
     const checker = () => {
-        const size = board.getSize;
+        const size = board.getSize();
         const victoryPoints = currentPlayer.id * size;
+
+        if (size*size === board.getFilledCells()) {
+            console.log(`Game board is full (Tied)`);
+            isGameFinished = true
+            return true
+        }
         // Row
         for (let i = 0; i < size; i++) {
             let sum = 0;
@@ -86,7 +110,11 @@ function GameController () {
                 sum += board.getCell(i, j);
             }
             console.log(`Row ${i + 1}: `, sum)
-            if (sum === victoryPoints) return console.log(`Player ${currentPlayer.name} won in row ${i + 1}`);
+            if (sum === victoryPoints) {
+                console.log(`Player ${currentPlayer.name} won in row ${i + 1}`);
+                isGameFinished = true;
+                return true;
+            }
         }
         // Column
         for (let i = 0; i < size; i++) {
@@ -94,8 +122,12 @@ function GameController () {
             for(let j = 0; j < size; j++) {
                 sum += board.getCell(j, i);
             }
-            console.log(`Column ${i + 1}: `, sum)
-            if (sum === victoryPoints) return console.log(`Player ${currentPlayer.name} won in column ${i + 1}`);
+            console.log(`Column ${i + 1}: `, sum);
+            if (sum === victoryPoints) {
+                console.log(`Player ${currentPlayer.name} won in column ${i + 1}`);
+                isGameFinished = true;
+                return true;
+            }
         }
         // Diagonal
         let diagonal1 = 0;
@@ -106,11 +138,14 @@ function GameController () {
         for (let i = 0; i < size; i++) {
             diagonal2 += board.getCell(i, (size - 1) - i);
         }
-        console.log(`Diagonal 1: `, diagonal1)
-        console.log(`Diagonal 2: `, diagonal2)
+        console.log(`Diagonal 1: `, diagonal1);
+        console.log(`Diagonal 2: `, diagonal2);
 
-        if (diagonal1 === victoryPoints) return console.log(`Player ${currentPlayer.name} won in diagonal`);
-        if (diagonal2 === victoryPoints) return console.log(`Player ${currentPlayer.name} won in diagonal`);
+        if (diagonal1 === victoryPoints || diagonal2 === victoryPoints) {
+            console.log(`Player ${currentPlayer.name} won in diagonal`);
+            isGameFinished = true;
+            return true;
+        }
     }
     
     return { playRound, getPlayer, getBoard: board.printBoard };
@@ -119,13 +154,13 @@ function GameController () {
 const controller = (function ScreenController () {
     const game = GameController();
     const gameBoardDiv = document.querySelector('.board');
-    const gamePlayerDiv = document.querySelector('.player')
+    const gamePlayerDiv = document.querySelector('.player');
 
     gameBoardDiv.addEventListener('click', (e) => {
         const element = e.target;
         if (!element.dataset.row) return;
-        game.playRound(element.dataset.row, element.dataset.column)
-        updateScreen()
+        let gameStatus = game.playRound(element.dataset.row, element.dataset.column);
+        updateScreen();
     })
 
     const updateScreen = () => {
@@ -133,18 +168,18 @@ const controller = (function ScreenController () {
         const currentBoard = game.getBoard();
         const fragment = document.createDocumentFragment();
         
-        gamePlayerDiv.textContent = `Player ${currentPlayer.name} turn (${currentPlayer.token})`
+        gamePlayerDiv.textContent = `Player ${currentPlayer.name} turn (${currentPlayer.token})`;
         for (let i = 0; i < 3; i++) {
             for(let j = 0; j < 3; j++) {
                 const cellBtn = document.createElement('button');
                 cellBtn.dataset.row = i;
                 cellBtn.dataset.column = j;
-                cellBtn.textContent = currentBoard[i][j]
+                cellBtn.textContent = currentBoard[i][j];
 
                 fragment.appendChild(cellBtn);
             }
         }
         gameBoardDiv.replaceChildren(fragment);
     }
-    updateScreen()
+    updateScreen();
 })();
